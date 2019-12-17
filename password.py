da032d47bd20c50a3265591664a020d94452c7d0
Alex Woods <alex.woods@student.manchester.ac.uk>
2019-12-17 18:05:28 +0000

    day 4

--- password.py
+++ password.py
@@ -12,15 +12,16 @@ def isValid(password):
     current = []
 
     last = None
+    hasDouble = False
+
+    prev = None
+    length = 0
+
     for c in pw: # check order
         if last != None and c < last:
             return False
         last = c
 
-    hasDouble = False
-
-    prev = None
-    length = 0
 
     for i in range (0, 6):
         if pw[i] == prev:
@@ -29,7 +30,9 @@ def isValid(password):
             if length == 2:
                 return True
             length = 1
+
         prev = pw[i]
+
 
     if length == 2:
         return True
@@ -48,11 +51,11 @@ def isValid(password):
 
 
 #print(isValid(111111))
-print("111122 T- ",isValid(111122))
-print("122789 T- ",isValid(122789))
-print("222289 F- ",isValid(222289))
-print("123369 T- ",isValid(123369))
-print("133369 F- ",isValid(133369))
+#print("111122 T- ",isValid(111122))
+#print("122789 T- ",isValid(122789))
+#print("222289 F- ",isValid(222289))
+#print("123369 T- ",isValid(123369))
+#print("133369 F- ",isValid(133369))
 
 while current < maximum:
     if isValid(current):
